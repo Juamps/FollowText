11f16c6ef24fcb2487abb3733d52ebf0f7a4cb0e
Juamps <juamps.delavega@gmail.com>
2016-06-02 10:44:15 -0500

    Improve sentence recognition and change output filename format

--- Source/follow_text_0.2.py
+++ Source/follow_text_0.2.py
@@ -5,6 +5,7 @@ from nltk.corpus import wordnet as wn  # get list of nouns
 from time import sleep, time  # performance metrics
 import codecs # handle utf8 characters
 import datetime # for timestamp
+import random # select random stuff
 
 
 MAX_CRAWL = 5
@@ -13,9 +14,10 @@ SENTENCES = []
 NOUNS = []
 WORD_FILENAME = "words"
 SENTENCE_FILENAME = "sentences"
-
+CONT = 0
 
 def list_nouns():
+    ## TODO CREATE A SEPARATE LIST FOR NOUNS ENDING IN S
     global NOUNS
     print "[+] Creating list of nouns... (This only has to be done once)"
     ## Make list of nouns in wordnet
@@ -26,7 +28,7 @@ def list_nouns():
 def next_word(pos=1):
     '''
     :param pos:
-    :return: returns the pos noun before the first period/stop [.]
+    :return: returns the [pos] noun before the first period/stop [.]
     '''
     global WORDS, NOUNS, SENTENCES, MAX_CRAWL
     ans = None
@@ -34,14 +36,24 @@ def next_word(pos=1):
     nouns_found = 0
     sentence = SENTENCES[-1]
     words = sentence.split(' ')
-    while not ans and cont < len(words):
+    while not ans and cont + 1 < len(words):
         ## select last word in sentence
         noun = words[-cont]
         ## remove periods, commas and plural forms ending in s from word
-        noun = noun.rstrip('.')
-        noun = noun.rstrip(',')
-        noun = noun.rstrip('\'')
-        noun = noun.lstrip('\'')
+        right_punctuation = ['.', ',', ')', ']', '}', '\'', 's', '\'', '\"']
+        left_punctuation = ['(', '[', '{', '\'', '\"']
+
+        for element in right_punctuation:
+            noun = noun.rstrip(element)
+        for element in left_punctuation:
+            noun = noun.lstrip(element)
+        # noun = noun.rstrip('.')
+        # noun = noun.rstrip(',')
+        # noun = noun.rstrip(')')
+        # noun = noun.rstrip('\'')
+        # noun = noun.lstrip('\'')
+        ## TODO CHECK FOR NOUNS ENDING IN S
+        noun = noun.lower()
         noun = noun.rstrip('s')
 
         ## check if word is noun
@@ -49,23 +61,20 @@ def next_word(pos=1):
             if len(noun) > 1:
                 if noun not in WORDS:
                     nouns_found += 1
-                    ## return word only if it is a non monosyllabic word and you have found 'pos - 1' new nouns before
+                    ## return word only if 'pos - 1' new nouns have been found before
                     if nouns_found == pos:
                         ans = noun
                 else:
                     print "[!] Word '", noun, "' already in list"
                     print "    Skipping..."
         cont += 1
-    ## crop sentence if necessary
-    if len(SENTENCES) < MAX_CRAWL and cont > 2:
-        sentence = ' '.join(words[0:2-cont])
-        SENTENCES[-1] = sentence
     return ans
 
 
 def validate_seed(seed):
     valid = False
     try:
+        print seed
         wikipedia.summary(seed)
         valid = True
     except wikipedia.exceptions.PageError as e:
@@ -81,8 +90,38 @@ def validate_seed(seed):
     return valid
 
 
+def disambiguate(word, e):
+    try:
+        ## Print all suggestions
+        # for option in e.options:
+        #     print option
+
+        # Choose a suggestion that explicitly has the word in it.
+        # suggestion = next(sug for sug in e.options if word.lower() in sug.lower())
+
+        ## Choose suggestion at random
+        suggestion = random.choice(e.options)
+    except StopIteration:
+        ## If none, choose first suggestion
+        suggestion = e.options[0]
+
+    try:
+        # print "Trying to use " + suggestion
+        ## Fetch new summary
+        summary = wikipedia.summary(suggestion)
+        print "[!] Ambiguous term, using:", suggestion
+        # print e.options
+        return summary
+    except wikipedia.exceptions.DisambiguationError as err:
+        # print err.options
+        return disambiguate(suggestion, err)
+    except wikipedia.exceptions.PageError as err:
+        print err.message
+        return disambiguate(suggestion, e)
+
+
 def add_word(word):
-    print "[+] Current search:", word, "  [", cont + 1, "/", MAX_CRAWL, "]"
+    print "[+] Current search:", word, "  [", CONT + 1, "/", MAX_CRAWL, "]"
     try:
         ## Fetch sentence
         summary = wikipedia.summary(word)
@@ -91,45 +130,81 @@ def add_word(word):
         if len(WORDS) == 0:
             print "[!]"
     except wikipedia.exceptions.DisambiguationError as e:
-        ## chose a suggestion that explicitly has the word in it. If none, choose first suggestion
-        try:
-            ## print all suggestions
-            # for option in e.options:
-            #     print option
+        summary = disambiguate(word, e)
 
-            suggestion = next(sug for sug in e.options if word.lower() in sug.lower())
-        except StopIteration:
-            suggestion = e.options[0]
+    summary_split = summary.split('.')
+    ## Clean summary
+    for elem in range(0, len(summary_split)):
+        summary_split[elem] = summary_split[elem].strip()
+    ## Keep first sentence and remove from summary
+    sentence = summary_split[0]
+    # if len(summary_split) > 1 and summary_split[1] != '':
+    #     summary_split = summary_split[1:]
+    #     ## Check if the first period does not end the sentence
+    #     ## Look at next sentence and if it doesn't start with a capital letter, append.
+    #     next_sentence = summary_split[0].lstrip()[0].strip()
+    #     while len(next_sentence) == 0 or (next_sentence.islower() and len(summary_split)) > 1:
+    #         sentence = sentence.rstrip() + '. ' + summary_split[0].lstrip()
+    #         summary_split = summary_split[1:]
+    #         next_sentence = summary_split[0].lstrip()[0].strip()
+    #     if summary_split[0].lstrip()[0].islower():
+    #         sentence = sentence.rstrip() + '. ' + summary_split[0].lstrip()
 
-        print "[!] Ambiguous term, using:", suggestion
-        # print e.options
-        summary = wikipedia.summary(suggestion)
+    if len(summary_split) > 1 and summary_split[1] != '':
+        summary_split = summary_split[1:]
+        ## Check if the first period does not end the sentence
+        ## Look at next sentence and if it doesn't start with a capital letter, append.
+        next_sentence = summary_split[0].strip()
+        # print summary_split
+        ## Check if length of next split is 1 => likely it is an acronym
+        if len(next_sentence) == 1:
+            while len(next_sentence) == 1 and len(summary_split) > 1:
+                sentence = sentence.rstrip() + '. ' + summary_split[0].lstrip()
+                summary_split = summary_split[1:]
+                next_sentence = summary_split[0].strip()
+            print next_sentence
+            ## Add the next sentence after the acronym
+            if len(next_sentence) > 1:
+                sentence = sentence.rstrip() + '. ' + summary_split[0].lstrip()
+        ## If not, check if it's some sort of abbreviation
+        else:
+            ## Sentences after periods would only be lower case if previous was abbreviation
+            next_sentence = summary_split[0].lstrip()[0].strip()
+            while next_sentence.islower() and len(summary_split) > 1 and summary_split[1] != '':
+                sentence = sentence.rstrip() + '. ' + summary_split[0].lstrip()
+                summary_split = summary_split[1:]
+                next_sentence = summary_split[0].lstrip()[0].strip()
+            if summary_split[0].lstrip()[0].islower():
+                sentence = sentence.rstrip() + '. ' + summary_split[0].lstrip()
 
-    ## Keep first sentence of summary
-    sentence = summary.split('.')[0]
+    ## Add final point to sentence
+    sentence = sentence.rstrip() + '.'
     ## Append term and sentence
     WORDS.append(word)
     SENTENCES.append(sentence)
-    print "    Sentence fetched."
-    print SENTENCES[-1]
+    print "    Sentence fetched:"
+    print "    " + SENTENCES[-1]
     print "\n"
 
 
 def write_to_file():
     global WORDS, SENTENCES, WORD_FILENAME, SENTENCE_FILENAME
-    ## add timestaps to file names
+
+    ## Add timestaps to file names
     timestamp = datetime.datetime.now().isoformat().split('.')[0]
-    wf = "../Output/" + WORD_FILENAME + timestamp
-    sf = "../Output/" + SENTENCE_FILENAME + timestamp
+    wf = "../Output/" + sys.argv[1] + "_" + str(CONT) + "_words_" + timestamp
+    sf = "../Output/" + sys.argv[1] + "_" + str(CONT) + "_summary_" + timestamp
     with codecs.open(wf, 'wb', "utf-8") as f:
         for word in WORDS:
-            f.write(word)
+            f.write(word + '\n')
 
     with codecs.open(sf, 'wb', "utf-8") as f:
         for sentence in SENTENCES:
-            f.write(sentence + '. ')
+            f.write(sentence + " ")
 
-if __name__ == '__main__':
+
+def main():
+    global MAX_CRAWL, CONT
     t = time()
     # Validate function call
     if len(sys.argv) != 3:
@@ -150,19 +225,21 @@ if __name__ == '__main__':
         MAX_CRAWL = int(sys.argv[2])
         if validate_seed(word):
             list_nouns()
-            cont = 0
+            CONT = 0
             print "[+] Initialising crawl."
-            while cont < MAX_CRAWL:
+            while CONT < MAX_CRAWL:
                 # pos = 1
                 add_word(word)
                 ## Choose next word
                 word = next_word()
                 # word = next_word(pos)
                 if not word:
+                    print "[*] There are no more nouns to follow. Exiting..."
                     break
-                cont += 1
-    except:
+                CONT += 1
+    except Exception as e:
         print "[!] Unexpected error", sys.exc_info()[0]
+        print "   ", e
     finally:
         # print "Words fetched:", WORDS
         # for s in SENTENCES:
@@ -173,3 +250,12 @@ if __name__ == '__main__':
         write_to_file()
         t = time() - t
         print "Time elapsed:", t
+
+if __name__ == '__main__':
+    main()
+
+
+
+"""
+
+"""
